e0aa8f4483d214d273474e7509c7d4e2dc138ea4
robgorhar <robgorhar@gmail.com>
2026-02-21 22:32:15 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "paroliamo-v1";
+
+const FILES_TO_CACHE = [
+  "paroliamo_deluxe.html",
+  "dizionario.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install: metto in cache i file base
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate: pulizia vecchie cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: prima rete, se fallisce uso cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
